a8aa35d571bd9b23fe4f2eb2e6405f7693607d15
Nick Visalli <nvisal1@students.towson.edu>
2020-03-23 22:58:13 -0400

    add JSDoc to controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,53 +8,30 @@ import { ApiBody, ApiOkResponse, ApiNotFoundResponse, ApiBadRequestResponse, Api
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiOkResponse({ description: 'Welcome to the CLARK Outcome Service' })
   @Get()
   @HttpCode(200)
   getHello(): string {
-    return this.appService.getHello();
+    return 'Welcome to the CLARK Outcome Service';
   }
 
-  /**
-   * User is not found - 404
-   * Learning Object is not found - 404
-   * Invalid username - 400
-   * Invalid Learning Object Id - 400
-   * If the object is unreleased and requester is not the author - 403
-   * If the object is waiting, review, or proofing and the requester is not privileged - 403
-   * Success - 200
-   */
-  @ApiOkResponse()
-  @ApiBadRequestResponse()
-  @ApiForbiddenResponse()
-  @ApiNotFoundResponse()
-  @Get('/users/:username/learning-objects/:loid/outcomes')
+  @ApiOkResponse({ description: 'OK' })
+  @ApiBadRequestResponse({ description: 'Invalid Learning Object ID || Invalid username' })
+  @ApiForbiddenResponse({ description: 'If the object is unreleased and requester is not the author || If the object is waiting, review, or proofing and the requester is not privileged' })
+  @ApiNotFoundResponse({ description: 'User is not found || Learning Object is not found' })
+  @Get('/users/:username/learning-objects/:learning-object-ID/outcomes')
   @HttpCode(200)
   getOutcomes(): any {
 
   }
 
-  /**
-   * 
-   * @param mappingWriteDTO 
-   * Success - 201
-   * Invalid bloom - 400
-   * Invalid verb - 400
-   * Username not found - 404
-   * Learning Object not found - 404
-   * Invalid username - 400
-   * Invalid Learning Object Id - 400
-   * If the Learning Object is unreleased and the requester is not the author - 403
-   * If the Learning Object is in waiting, review, or proofing and requester is not privileged - 403
-   * If the object is released - 403
-   * If the requester is not signed in - 401
-   */
-  @ApiCreatedResponse()
-  @ApiBadRequestResponse()
-  @ApiUnauthorizedResponse()
-  @ApiForbiddenResponse()
-  @ApiNotFoundResponse()
+  @ApiCreatedResponse({ description: 'Created' })
+  @ApiBadRequestResponse({ description: 'Invalid bloom || Invalid verb || Invalid username || Invalid Learning Object ID'})
+  @ApiUnauthorizedResponse({ description: 'If the requester is not signed in' })
+  @ApiForbiddenResponse({ description: 'If the Learning Object is unreleased and the requester is not the author || If the Learning Object is in waiting, review, or proofing and requester is not privileged || If the object is released' })
+  @ApiNotFoundResponse({ description: 'Username not found || Learning Object not found'})
   @ApiBody({ type: OutcomeWriteDTO })
-  @Post('/users/:username/learning-objects/:loid/outcomes')
+  @Post('/users/:username/learning-objects/:learning-object-ID/outcomes')
   @UsePipes(ValidationPipe)
   @HttpCode(201)
   addOutcome(@Body() outcomeWriteDTO: OutcomeWriteDTO): any {
@@ -68,7 +45,7 @@ export class AppController {
   @ApiNotFoundResponse({ description: 'Username not found || Learning Object not found || Outcome not found' })
   @ApiConflictResponse({ description: 'Update would create a duplicate' })
   @ApiBody({ type: OutcomeWriteDTO })
-  @Patch('/users/:username/learning-objects/:loid/outcomes/:outcomeId')
+  @Patch('/users/:username/learning-objects/:learning-object-ID/outcomes/:outcome-ID')
   @UsePipes(ValidationPipe)
   @HttpCode(204)
   updateOutcome(@Body() outcomeWriteDTO: OutcomeWriteDTO): void {
@@ -80,7 +57,8 @@ export class AppController {
   @ApiUnauthorizedResponse({ description: 'If the requester is not signed in'})
   @ApiForbiddenResponse({ description: 'If Learning Object is released || If Learning Object is in waiting, review, proofing and requester is not privileged || If Learning Object is unreleased and requester is not the author' })
   @ApiNotFoundResponse({ description: 'Username not found || Learning Object not found ||  Outcome not found' })
-  @Delete('/users/:username/learning-objects/:loid/outcomes/:outcomeId')
+  @Delete('/users/:username/learning-objects/:learning-object-ID/outcomes/:outcome-ID')
+  @HttpCode(204)
   deleteOutcome(): any {
 
   }
@@ -92,7 +70,7 @@ export class AppController {
   @ApiNotFoundResponse({ description: 'Username not found || Learning Object not found || Outcome not found || Guideline not found' })
   @ApiConflictResponse({ description: 'Update would create a duplicate'})
   @ApiBody({ type: MappingWriteDTO })
-  @Post('/users/:username/learning-objects/:loid/outcomes/:outcomeId/mappings')
+  @Post('/users/:username/learning-objects/:learning-object-ID/outcomes/:outcome-ID/mappings')
   @UsePipes(ValidationPipe)
   @HttpCode(204)
   addMapping(@Body() mappingWriteDTO: MappingWriteDTO): any {
@@ -104,7 +82,7 @@ export class AppController {
   @ApiUnauthorizedResponse({ description: 'The requester is not signed in' })
   @ApiForbiddenResponse({ description: 'If Learning Object is in waiting, review, proofing and requester is not privileged || If the Learning Object is released and the requester is not a mapper, editor, or admin || If Learning Object is unreleased and requester is not the author'})
   @ApiNotFoundResponse({ description: 'Username not found || Learning Object not found || Outcome not found || Mapping not found'})
-  @Delete('/users/:username/learning-objects/:loid/outcomes/:outcomeId/mappings/:mappingId')
+  @Delete('/users/:username/learning-objects/:learning-object-ID/outcomes/:outcome-ID/mappings/:mapping-ID')
   @HttpCode(204)
   deleteMapping(): any {
 
