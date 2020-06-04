a91f7cf7dc86291f94913a455359f47ac8323454
pzalep1 <pz0342@gmail.com>
2020-06-04 12:10:17 -0400

    Add guidelineDTO

---
+++ src/DTO/GuidelineReadDTO.ts
@@ -0,0 +1,9 @@
+export class GuidelineDTO {
+    _id: string;
+    author: string;
+    date: number;
+    outcome: string;
+    source: string;
+    tag: string;
+    name: string;
+}
--- src/DTO/OutcomeRead.DTO.ts
+++ src/DTO/OutcomeRead.DTO.ts
@@ -1,8 +1,10 @@
+import { GuidelineDTO } from "./GuidelineReadDTO";
+
 export class OutcomeReadDTO {
     _id: string;
     bloom: string;
     verb: string;
     text: string;
     lastUpdated: Date;
-    mappings: [string]
+    // mappings: [GuidelineDTO];
 }
